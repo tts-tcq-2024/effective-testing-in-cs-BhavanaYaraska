97f0d1bcc9ab604435e97c666c7bf28dc6f033fc
Bhavana Yaraskar <bhavanayaraskar@gmail.com>
2024-08-02 18:54:11 +0530

    Update tshirts.cs

--- tshirts/tshirts.cs
+++ tshirts/tshirts.cs
@@ -6,17 +6,19 @@ namespace TshirtSpace {
         static string Size(int cms) {
             if(cms < 38) {
                 return "S";
-            } else if(cms > 38 && cms < 42) {
+            } else if(cms >= 38 && cms <= 42) { // Changed condition to include boundary value
                 return "M";
             } else {
                 return "L";
             }
         }
+
         static void Main(string[] args) {
             Debug.Assert(Size(37) == "S");
+            Debug.Assert(Size(38) == "M"); // Added boundary value
             Debug.Assert(Size(40) == "M");
             Debug.Assert(Size(43) == "L");
-            Console.WriteLine( "All is well (maybe!)");
+            Console.WriteLine("All is well (maybe!)");
         }
     }
 }
